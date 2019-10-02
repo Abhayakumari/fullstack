942318d06842afd9f5e2efc54b0ab612c67d1cf0
abhaya <abhayacse@gmail.com>
2019-10-02 11:25:24 +0530

    some js files

---
+++ app.js
@@ -0,0 +1,18 @@
+function test(){
+    console.log("Hello world!")
+}
+test();
+
+alert('alert here');
+confirm("confirm here");
+window.confirm("confirm here");
+prompt("prompt here");
+document.write("hello");
+document.body.innerHTML("inner html");
+document.body.innerText("inner text");
+
+
+//This is for window object...valid only for browser.
+
+
+
